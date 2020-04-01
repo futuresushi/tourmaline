bdd8574d22f267e98a1e6e83a577b84b4f996006
futuresushi <mattsparkzz@gmail.com>
2020-03-31 21:27:37 -0400

    Removing gap skip for TM counting

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -260,7 +260,7 @@ ENDM
 	add_tm FIRE_PUNCH   ; f0
 	add_tm FURY_CUTTER  ; f1
 	add_tm NIGHTMARE    ; f2
-NUM_TMS EQU const_value - TM01 - 2 ; discount ITEM_C3 and ITEM_DC
+NUM_TMS EQU const_value - TM01 ; discount ITEM_C3 and ITEM_DC
 
 add_hm: MACRO
 if !DEF(HM01)
