e1e30c975c2d1800f480412c4dab0df6e779528d
futuresushi <mattsparkzz@gmail.com>
2020-04-01 21:21:28 -0400

    Trying something

--- macros/scripts/battle_commands.asm
+++ macros/scripts/battle_commands.asm
@@ -110,9 +110,9 @@ ENDM
 	command batonpass               ; 67
 	command pursuit                 ; 68
 	command clearhazards            ; 69
-	command healmorn                ; 6a
-	command healday                 ; 6b
-	command healnite                ; 6c
+	command healweather             ; 6a
+	command healunused              ; 6b
+	command healunused              ; 6c
 	command hiddenpower             ; 6d
 	command startrain               ; 6e
 	command startsun                ; 6f
