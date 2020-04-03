6ae201d6101a14f409c3d11932f65f9915a5e88c
futuresushi <mattsparkzz@gmail.com>
2020-04-02 20:12:09 -0400

    Updating music constants for added songs
    Added: Abandoned Ship, Littleroot Town, Trick House and Mart music from R/S/E
    Added: Cipher Peon Battle from Pokemon XD
    Source: https://soundcloud.com/mmmmmmmmmmmmmmmmm-1

--- constants/music_constants.asm
+++ constants/music_constants.asm
@@ -107,6 +107,14 @@
 	const MUSIC_SUICUNE_BATTLE               ; 64
 	const MUSIC_BATTLE_TOWER_LOBBY           ; 65
 	const MUSIC_MOBILE_CENTER                ; 66
+	
+; custom music
+
+	const MUSIC_ABANDONED_SHIP               ; 67
+	const MUSIC_CIPHER_PEON                  ; 68
+	const MUSIC_LITTLEROOT_TOWN              ; 69
+	const MUSIC_MART_RS                      ; 6a
+	const MUSIC_TRICK_HOUSE                  ; 6b
 
 ; GetMapMusic picks music for these values (see home/map.asm)
 MUSIC_MAHOGANY_MART EQU $fc
