a2f1ef81607febe87c63ecdb75501537c88b4f55
futuresushi <mattsparkzz@gmail.com>
2020-04-04 19:01:48 -0400

    Adding new battle command 'calmmind'

--- macros/scripts/battle_commands.asm
+++ macros/scripts/battle_commands.asm
@@ -180,6 +180,7 @@ ENDM
 	command supereffectivelooptext  ; ad
 	command startloop               ; ae
 	command curl                    ; af
+	command calmmind		; b1
 
 	enum_start $fe
 	command endturn                 ; fe
