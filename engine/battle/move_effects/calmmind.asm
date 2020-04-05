cbb214c18d0ef6daaf36aac314d671e7b83fd46e
futuresushi <mattsparkzz@gmail.com>
2020-04-05 00:32:12 -0400

    Trying something, revert if busted

--- engine/battle/move_effects/calmmind.asm
+++ engine/battle/move_effects/calmmind.asm
@@ -1,47 +1,13 @@
 BattleCommand_CalmMind:
 ; calm mind
 
-	ld de, wBattleMonType1
-	ld bc, wPlayerStatLevels
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .go
-	ld de, wEnemyMonType1
-	ld bc, wEnemyStatLevels
+BattleCommand_SpecialAttackUp:
+; specialattackup
+	ld b, SP_ATTACK
+	jr BattleCommand_StatUp
 
-.go
-
-
-; Attack
-	ld a, [bc]
-	cp MAX_STAT_LEVEL
-	jr c, .raise
-
-; Defense
-	inc bc
-	ld a, [bc]
-	cp MAX_STAT_LEVEL
-	jr nc, .cantraise
-
-.raise
-
-; Raise Special stats
-
-	ld a, $1
-	ld [wKickCounter], a
-	call AnimateCurrentMove
-	call BattleCommand_SwitchTurn
-	call BattleCommand_SpecialAttackUp
-	call BattleCommand_StatUpMessage
-	call ResetMiss
-	call BattleCommand_SpecialDefenseUp
-	jp BattleCommand_StatUpMessage
-
-.cantraise
-
-; Can't raise either stat.
-
-	call GetStatName
-	call AnimateFailedMove
-	ld hl, WontRiseAnymoreText
-	jp StdBattleTextbox
+BattleCommand_SpecialDefenseUp:
+; specialdefenseup
+	ld b, SP_DEFENSE
+	jr BattleCommand_StatUp
+	
