e90f10e1891d45f7f87d6f323142166ca6877ca8
futuresushi <mattsparkzz@gmail.com>
2020-04-05 14:17:15 -0400

    Changing Mew debug moves
    Will-O-Wisp > Thunder Wave
    Nasty Plot restored to Lv 20 learn

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -2060,8 +2060,8 @@ MewEvosAttacks:
 	db 0 ; no more evolutions
 	db 1, POUND
 	db 10, TRANSFORM
-	db 22, NASTY_PLOT ; switch back to 20
-	db 22, WILL_O_WISP ; temp, remove once tested
+	db 20, NASTY_PLOT
+	db 23, THUNDER_WAVE ; temp, remove once tested
 	db 30, METRONOME
 	db 40, PSYCHIC_M
 	db 50, ANCIENTPOWER
