99156e0f4a028385dfef6c0e740e99ccbcc5d047
futuresushi <mattsparkzz@gmail.com>
2020-04-29 00:57:40 -0400

    Steel Wing TM > Metal Coat

--- maps/RockTunnel1F.asm
+++ maps/RockTunnel1F.asm
@@ -10,7 +10,7 @@ RockTunnel1F_MapScripts:
 RockTunnel1FElixer:
 	itemball ELIXER
 
-RockTunnel1FTMSteelWing:
+RockTunnel1FMetalCoat:
 	itemball METAL_COAT
 
 RockTunnel1FHiddenXAccuracy:
@@ -38,4 +38,4 @@ RockTunnel1F_MapEvents:
 
 	db 2 ; object events
 	object_event  4, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FElixer, EVENT_ROCK_TUNNEL_1F_ELIXER
-	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FTMSteelWing, EVENT_ROCK_TUNNEL_1F_TM_STEEL_WING
+	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FMetalCoat, EVENT_ROCK_TUNNEL_1F_METAL_COAT
